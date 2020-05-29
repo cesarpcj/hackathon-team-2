120e8e76e07220aa7f91f0977e2d97e25a8a2551
cesarpcj <40581793+cesarpcj@users.noreply.github.com>
2020-05-29 15:00:57 +0100

    Update post.js

--- models/post.js
+++ models/post.js
@@ -2,67 +2,88 @@
 
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  name: {
+    type: String,
+    minlength: 1,
+    maxlength: 20,
+  },
+
+  text: {
+    type: String,
+  },
+});
+
 const postSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       minlength: 1,
       maxlength: 20,
-      required: true
+      required: true,
     },
     description: {
       type: String,
       minlength: 1,
       maxlength: 200,
-      required: true
+      required: true,
     },
     like_count: {
       type: Number,
-      default: 0
+      default: 0,
     },
-    user_liked: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        // unique: true,
-        ref: "User"
-      }
-    ],
+    // user_liked: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     required: true,
+    //     // unique: true,
+    //     ref: "User"
+    //   }
+    // ],
+
+    pictureUrl: {
+      type: String,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    comments: [commentSchema],
     location: {
       type: {
         type: String,
         default: "Point",
-        required: true
+        required: true,
       },
       coordinates: [
         {
           type: mongoose.Schema.Types.Decimal128,
           min: -180,
-          max: 180
-        }
-      ]
+          max: 180,
+        },
+      ],
     },
-    pictureUrl: {
-      type: String
-    },
-    creator: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User"
-    }
   },
+
   {
     timestamps: {
       /*currentTime: () => Math.floor(Date.now() / 1000)*/
       createdAt: "createdDate",
-      updatedAt: "updatedDate"
-    }
+      updatedAt: "updatedDate",
+    },
   },
   {
     time: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+    },
   }
 );
 
